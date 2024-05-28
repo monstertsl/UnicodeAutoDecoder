d974fb9b2ac4e3ac42f77ea00f8ef63acae97267
monstertsl <39371736+monstertsl@users.noreply.github.com>
2024-05-28 15:04:41 +0800

    Add files via upload

--- UnicodeAutoDecoder.py
+++ UnicodeAutoDecoder.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from burp import IBurpExtender, IHttpListener
 import re
 
@@ -8,13 +9,12 @@ class BurpExtender(IBurpExtender, IHttpListener):
 		self._helpers = callbacks.getHelpers()
 		callbacks.setExtensionName("UnicodeAutoDecoder")
 		callbacks.registerHttpListener(self)
-
 		callbacks.printOutput("UnicodeAutoDecoder By monstertsl")
-		callbacks.printOutput("自动将抓包、重放、爆破中的Unicode编码转换为易于阅读的自然语言")
+		callbacks.printOutput("Auto converts Unicode encodings in Proxy, Repeater, and Intruder into easy-to-read natural language")
 		callbacks.printOutput("GitHub: https://github.com/monstertsl/UnicodeAutoDecoder")
-		callbacks.printOutput("参考一下两位作者项目进行创建")
+		callbacks.printOutput("Refer to the following two authors create project")
 		callbacks.printOutput("GitHub: https://github.com/amir-h-fallahi/UnicodeDecoder")
-		callbacks.printOutput("GitHub：https://github.com/no001ce/N-DecodeAllUnicode")
+		callbacks.printOutput("GitHub: https://github.com/no001ce/N-DecodeAllUnicode")
 
 
 	def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
